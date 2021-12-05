7152a3d12c0b984e2ec7ce9b1c73672d5ea31114
meyerjas <meyer.jasmin1099@gmail.com>
2021-12-05 20:31:09 +0100

    added comment on regEx

--- src/classes/registrationManager.ts
+++ src/classes/registrationManager.ts
@@ -4,6 +4,7 @@ export class RegistrationManager {
     public path: String;
 
     public register(username: String, password: String): User {
+        //will accept any password between 4 and 8 signs, must contain numbers. 
         password.match(/^(?=.*\d).{4,8}$/);
     }
     public login(username: String, password: String): User {
