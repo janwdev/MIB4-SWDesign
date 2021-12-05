e81444ded46c5b49eca0a6fb6412085e77f6bb92
meyerjas <meyer.jasmin1099@gmail.com>
2021-12-05 20:33:46 +0100

    further change of comment

--- src/classes/registrationManager.ts
+++ src/classes/registrationManager.ts
@@ -4,7 +4,7 @@ export class RegistrationManager {
     public path: String;
 
     public register(username: String, password: String): User {
-        //will accept any password between 4 and 8 signs, must contain numbers. 
+        //will accept any password between 4 and 8 signs consisting of numbers or letters, must contain numbers. 
         password.match(/^(?=.*\d).{4,8}$/);
     }
     public login(username: String, password: String): User {
